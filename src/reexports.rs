2f37c8a3be45cba824519563bfc4dff9d7eb2e21
OpenByte <development.openbyte@gmail.com>
2025-05-28 16:12:11 +0200

    Remove doc_cfg feature

--- src/reexports.rs
+++ src/reexports.rs
@@ -1,9 +1,9 @@
 pub use deadpool;
 pub use fantoccini;
 pub use hyper;
-#[cfg(any(feature = "rustls-tls", feature = "doc_cfg"))]
-#[cfg_attr(feature = "doc_cfg", doc(cfg(feature = "rustls-tls")))]
+#[cfg(any(feature = "rustls-tls", docsrs))]
+#[cfg_attr(docsrs, doc(cfg(feature = "rustls-tls")))]
 pub use hyper_rustls;
-#[cfg(any(feature = "native-tls", feature = "doc_cfg"))]
-#[cfg_attr(feature = "doc_cfg", doc(cfg(feature = "native-tls")))]
+#[cfg(any(feature = "native-tls", docsrs))]
+#[cfg_attr(docsrs, doc(cfg(feature = "native-tls")))]
 pub use hyper_tls;
