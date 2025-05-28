e4f27f3382e6e2e253b5f270fe50d3690cf40a2a
OpenByte <development.openbyte@gmail.com>
2025-05-28 16:25:36 +0200

    Set webdriver port to 4444

--- ci/windows-latest-chrome.ps1
+++ ci/windows-latest-chrome.ps1
@@ -1,5 +1,5 @@
 npm i puppeteer
 npx @puppeteer/browsers install chrome@stable
 npx @puppeteer/browsers install chromedriver@stable
-Start-Process -FilePath chromedriver -ArgumentList "--port=9515"
+Start-Process -FilePath chromedriver -ArgumentList "--port=4444"
 Start-Sleep -Seconds 1
